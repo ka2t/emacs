9d2bb5d28f09b0901c2e3f54b262f7b274705220
Kani Takanori <ka2@noroi.local>
2018-08-13 22:42:04 +0900

    Add helm1

---
+++ inits/11_helm.el
@@ -0,0 +1,33 @@
+(use-package helm
+  :config
+  (use-package helm-descbinds)
+  (use-package helm-gtags)
+  
+  :bind(
+  ("M-x" . helm-M-x)
+  ("C-x /" . helm-resume)
+  ("C-x C-f" . helm-find-files)
+  ("C-x C-r" . helm-recentf)
+  ("M-y" . helm-show-kill-ring)
+  ("C-x i" . helm-imenu)
+  ("C-x b" . helm-buffers-list)
+  ("C-c g" . helm-ag)
+  ("M-'" . helm-ag-pop-stack)
+  ("C-;" . helm-for-files)
+  ("C-'" . helm-browse-project)
+  ("C-x r l" . helm-bookmarks)
+  ("M-." . gtags-find-tag-from-here)
+  ("M-," . helm-gtags-pop-stack)))
+
+  ;; :bind (:map helm-map
+  ;;        ("C-z" . helm-select-action)
+  ;;        ("C-h" . delete-backward-char)
+  ;;        :map helm-find-files-map
+  ;;        ("C-h" . delete-backward-char)
+  ;;        ("TAB" . helm-execute-persistent-action)
+  ;;        :map helm-read-file-map
+  ;;        ("TAB" . helm-execute-persistent-action))
+
+
+  ;; 候補のディレクトリが一つしかない場合に、自動的に展開しない
+  ;;  (setq helm-ff-auto-update-initial-value nil))
